8c4e0b8e464638ab3e41fcfc210361e36611d4cb
LewisConrad <up2120028@myport.ac.uk>
2025-12-15 15:30:22 +0000

    funcs

--- py/functions.py
+++ py/functions.py
@@ -1,1 +1,19 @@
-
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler , OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.tree import plot_tree
+from sklearn import tree
+import torch
+import torch.nn as nn 
+import torch.optim as optim
+from torch.utils.data import TensorDataset, DataLoader, Dataset 
+import os
+def load_dataset(path='/workspaces/up2107120-ACT-CW2-repo/space-missions-dataset/space_missions_dataset.csv'):
+  return pd.read_csv(path)
