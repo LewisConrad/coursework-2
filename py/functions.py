0721f2593a4a01602903f22d88318d4bc411685c
LewisConrad <up2120028@myport.ac.uk>
2025-12-16 11:56:15 +0000

    moving files

--- py/functions.py
+++ py/functions.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
@@ -10,10 +10,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.tree import plot_tree
 from sklearn import tree
-import torch
-import torch.nn as nn 
-import torch.optim as optim
-from torch.utils.data import TensorDataset, DataLoader, Dataset 
+
 import os
 def load_dataset(path='/workspaces/coursework-2/tested.csv'):
   return pd.read_csv(path)
